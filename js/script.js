1b6a113938bf34440c2f1323d4a2b81dba90f80f
justcivah <me@giovanninovati.com>
2023-11-16 11:51:53 +0100

    Navbar is now created via javascript

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ function addEmailAddress() {
     
     document.getElementById('contact-email').setAttribute('href', 'mailto:' + username + '@' + domain + '.' + suffix);
 }
+
+function printNavbar() {
+    document.getElementById('navbar').innerHTML = `
+        <div class="row gy-2">
+            <div class="col-12 col-sm-6 text-center text-sm-start">
+                <a class="user-select-none text-decoration-none my-link my-navbar-text" href="#">Giovanni Novati</a>
+            </div>
+            <div class="col-12 col-sm-6">
+                <div class="row justify-content-center justify-content-sm-end">
+                    <div class="col-auto">
+                        <a class="user-select-none my-link my-navbar-text" href="http://localhost:5500/about/about.html">About</a>
+                    </div>
+                    <div class="col-auto">
+                        <a class="user-select-none my-link my-navbar-text" href="http://localhost:5500/blog/blog.html">Blog</a>
+                    </div>
+                    <div class="col-auto">
+                        <a id="contact-email" class="user-select-none my-link my-navbar-text">Contacts</a>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
